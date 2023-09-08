b5571b6abf00be46ae08b0f5db33405db262f940
AnkitaBansa <141407876+AnkitaBansa@users.noreply.github.com>
2023-09-08 21:07:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header';
+import CardList from './CardList';
+
+function App() {
+  return (
+    <div>
+      <Header text='Staff list' />
+      <CardList />
+    </div>
+  );
+}
+
+export default App;
